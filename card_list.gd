7e59949aa73d121d4c37b27279b6a1e5c9cb32c4
SeppNel <josenova1000@gmail.com>
2025-03-10 11:24:02 +0100

    Show promo last

--- card_list.gd
+++ card_list.gd
@@ -108,16 +108,16 @@ func loadCards():
 		var spaceTimeCards = DbManager.getSpaceTimeCards(parse_order())
 		addCollectionTitle("res://img/space_time_smackdown.webp")
 		addCollectionCards(spaceTimeCards, gotCards)
-			
-	if m_collectionFilter == 4 or m_collectionFilter == 0:
-		var promoCards = DbManager.getPromoCards()
-		addCollectionTitle("res://img/promo_a.webp")
-		addCollectionCards(promoCards, gotCards)
 		
 	if m_collectionFilter == 5 or m_collectionFilter == 0:
 		var tlCards = DbManager.getTriumphantLightCards(parse_order())
 		addCollectionTitle("res://img/triumphant_light.webp")
 		addCollectionCards(tlCards, gotCards)
+	
+	if m_collectionFilter == 4 or m_collectionFilter == 0:
+		var promoCards = DbManager.getPromoCards()
+		addCollectionTitle("res://img/promo_a.webp")
+		addCollectionCards(promoCards, gotCards)
 
 func clearCardList():
 	for child in get_children():
